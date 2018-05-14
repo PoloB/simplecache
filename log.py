8c0a3b687a1fa614c524f00ee335052069bf30b5
pe <paulemilebuteau@gmail.com>
2018-05-14 21:10:26 +0200

    lets make it simple

--- log.py
+++
@@ -1,5 +0,0 @@
-
-import logging.config
-
-log = logging.getLogger('vgppet')
-logging.basicConfig(level=logging.INFO)
--- test_cache.py
+++ test_cache.py
@@ -4,34 +4,22 @@ import unittest
 from cache import CacheManager
 
 
-def first_arg(a, k): return a[0]
-
-
-def direct_out(o): return o
+def first_arg(a):
+    return a
 
 
 class TestCache(unittest.TestCase):
-    cache_manager = CacheManager(['first_id', 'second_id'])
+    cache_manager = CacheManager()
 
     @staticmethod
-    @cache_manager.cache('first_id', first_arg, direct_out)
+    @cache_manager.cache()
     def from_first_id(first_id):
         return {'first_id': 1, 'second_id': 'second_1'}
 
     @staticmethod
-    @cache_manager.cache('second_id', first_arg, direct_out)
-    def from_second_id(second_id):
+    @cache_manager.cache(first_arg)
+    def from_first_id(first_id):
         return {'first_id': 1, 'second_id': 'second_1'}
-
-    @staticmethod
-    @cache_manager.cache('first_id', first_arg, direct_out)
-    def from_first_id_but_no_second(first_id):
-        return {'first_id': 2}
-
-    @staticmethod
-    @cache_manager.cache('first_id', first_arg, direct_out)
-    def from_first_id_but_no_first(first_id):
-        return {'no_first_id': 3}
 
     def test_cache_adding(self):
         # Test caching works
@@ -40,19 +28,8 @@ class TestCache(unittest.TestCase):
         # The cache must contains something
 
         inst_from_cache = TestCache.from_first_id(1)
-        inst_from_side = TestCache.from_second_id('second_1')
 
-        assert inst1 is inst_from_cache is inst_from_side
-
-        inst2 = TestCache.from_first_id_but_no_second(2)
-        inst_from_cache2 = TestCache.from_first_id(2)
-
-        assert inst2 is inst_from_cache2
-
-        with self.assertRaises(AssertionError) as ctx:
-            TestCache.from_first_id_but_no_first(3)
-
-        self.assertTrue("No primary key in result." in str(ctx.exception))
+        assert inst1 is inst_from_cache
 
     def test_cache_removal(self):
         # All cache delete
